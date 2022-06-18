dc6942b8d1ba726a747360b10ad3138ba68c5292
slaweksystem <94764770+slaweksystem@users.noreply.github.com>
2022-06-18 14:31:34 +0200

    Update RetroArch-UpdateConfig.ps1
    Complete script - we will fix some issues later

--- RetroArch-UpdateConfig.ps1
+++ RetroArch-UpdateConfig.ps1
@@ -1,3 +1,5 @@
+$RetroArchPath = "C:\RetroArch"
+
 echo "RetroArch Slav installator"
 
 echo "There will be a lot of downloading, internet is required :)"
@@ -7,7 +9,7 @@ echo "Check if git is enabled"
 try
 {
     git | Out-Null
-   "Git zainstalowany"
+   "Git installed :)"
 }
 catch [System.Management.Automation.CommandNotFoundException]
 {
@@ -19,3 +21,11 @@ catch [System.Management.Automation.CommandNotFoundException]
     $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
 
 }
+
+if (Test-Path "C:\RetroArch")
+{
+    echo "Removing last RetroArch"
+    Remove-Item $RetroArchPath -Recurse -Force
+}
+
+git clone --branch main https://github.com/slaweksystem/RetroArch.git $RetroArchPath
